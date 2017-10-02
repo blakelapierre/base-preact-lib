8060294507f32bc459caca1e21b30f9cd59d49ac
Blake La Pierre <blakelapierre@gmail.com>
2017-10-02 13:20:13 -0400

    Sample export and built

--- .dist/index.js
+++ .dist/index.js
@@ -1,10 +1,18 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Sample = undefined;
+
 var _sample = require('./sample');
 
 var _sample2 = _interopRequireDefault(_sample);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.Sample = _sample2.default;
+
 
 console.log(new _sample2.default());
 //# sourceMappingURL=index.js.map
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Sample from './sample';
 
+export {Sample};
 
 console.log(new Sample());
